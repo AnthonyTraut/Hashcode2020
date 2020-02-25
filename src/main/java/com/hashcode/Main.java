2156c6e95ca972b74e7b8bdc09512193483e98ab
Anthony Traut <traut.anthony@gmail.com>
2020-02-25 13:18:32 +0100

    v1

--- src/main/java/com/hashcode/Main.java
+++ src/main/java/com/hashcode/Main.java
@@ -11,7 +11,6 @@ import com.hashcode.utils.OutputFile;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -20,19 +19,17 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
 
     public static void main(String[] args) {
-        final List<Integer> results = new ArrayList<>();
+        AtomicInteger scoreTotal = new AtomicInteger();
 
         Arrays.asList(args).parallelStream().forEach(file -> {
-            results.add(Main.process(file));
+            scoreTotal.addAndGet(Main.process(file));
         });
 
-        AtomicInteger scoreTotal = new AtomicInteger();
-        results.forEach(scoreTotal::addAndGet);
+
         log.info("Score total : " + scoreTotal.get());
     }
 
     private static int process(final String file) {
-        long startTime = System.currentTimeMillis();
         final FileUtils fileUtils = new FileUtils(file);
         fileUtils.getInputFile();
 
